ea8ea81d99c1d94d37c325ba4cd73bc01279775c
Sergio Solorzano Rocha <sosergio@stud.phys.ethz.ch>
2016-04-02 11:55:10 +0200

    fixed d parameter update

--- rbm/rbm.c
+++ rbm/rbm.c
@@ -41,38 +41,38 @@ void gibbs_X(int * x, int * h0);
 
 double uniform()
 {
-    return rand()/(RAND_MAX + 1.0);
+    return rand() / (RAND_MAX + 1.0);
 }
 
 
 int ReverseInt (int i)
-{   
+{
     unsigned char ch1, ch2, ch3, ch4;
     ch1 = i & 255;
     ch2 = (i >> 8) & 255;
     ch3 = (i >> 16) & 255;
     ch4 = (i >> 24) & 255;
-    return((int) ch1 << 24) + ((int)ch2 << 16) + ((int)ch3 << 8) + ch4;
+    return ((int) ch1 << 24) + ((int)ch2 << 16) + ((int)ch3 << 8) + ch4;
 }
 
-void image_pp(){
-    
-    images_train =(int *)malloc(sizeof(int)*D*num_img_train);
-    images_test =(int *)malloc(sizeof(int)*D*num_img_test);
+void image_pp() {
 
-    for(int i=0; i< num_img_train*D; i++)
+    images_train = (int *)malloc(sizeof(int) * D * num_img_train);
+    images_test = (int *)malloc(sizeof(int) * D * num_img_test);
+
+    for (int i = 0; i < num_img_train * D; i++)
     {
-        
-        images_train[i]=  (int)images_train_init[i] <127 ? 0 : 1;
-        
+
+        images_train[i] =  (int)images_train_init[i] < 127 ? 0 : 1;
+
     }
-    
-    
-        for(int i=0; i< num_img_test*D; i++)
+
+
+    for (int i = 0; i < num_img_test * D; i++)
     {
-        
-        images_test[i]=  (int)images_test_init[i] <127 ? 0 : 1;
-        
+
+        images_test[i] =  (int)images_test_init[i] < 127 ? 0 : 1;
+
     }
 
 
@@ -86,34 +86,34 @@ void init_param()
     c = (double *) malloc(n * sizeof(double));
     d = (double *) malloc(K * sizeof(double));
     U = (double *) malloc(n * K * sizeof(double));
-    
+
     //The biases b ,c ,d are initiallez to zero
-    
-    for(int i=0;i<D;i++){
-        b[i]=0;
+
+    for (int i = 0; i < D; i++) {
+        b[i] = 0;
     }
-    
-    for(int i=0;i<n;i++){
-        c[i]=0;
+
+    for (int i = 0; i < n; i++) {
+        c[i] = 0;
     }
-    
-    for(int i=0;i<K;i++){
-        d[i]=0;
+
+    for (int i = 0; i < K; i++) {
+        d[i] = 0;
     }
-    
+
     /*
      The matrices are initialized with a random number
      in the range [-m^0.5,m^0.5] where m is the maximum between the number of rows and columns of the matrix.
      */
-    
-    double maxW = n<D?1.0/sqrt(D):1.0/sqrt(n);
-    for(int i=0 ;i<n*D;i++){
-        W[i]= (uniform()-0.5)*maxW;
+
+    double maxW = n < D ? 1.0 / sqrt(D) : 1.0 / sqrt(n);
+    for (int i = 0 ; i < n * D; i++) {
+        W[i] = (uniform() - 0.5) * maxW;
     }
-    
-    double maxU = n<K?1.0/sqrt(K):1.0/sqrt(n);
-    for(int i=0 ;i<n*K;i++){
-        U[i]= (uniform()-0.5)*maxU;
+
+    double maxU = n < K ? 1.0 / sqrt(K) : 1.0 / sqrt(n);
+    for (int i = 0 ; i < n * K; i++) {
+        U[i] = (uniform() - 0.5) * maxU;
     }
 
 }
@@ -129,14 +129,14 @@ void get_images(int *_num_img, unsigned char **_images, int **_labels, char *ima
     unsigned char *images;
     int *labels;
 
-    FILE *image_file = fopen(imagesFileName,"rb");
-    FILE *label_file = fopen(labelsFileName,"rb");
-    if(!image_file) {
+    FILE *image_file = fopen(imagesFileName, "rb");
+    FILE *label_file = fopen(labelsFileName, "rb");
+    if (!image_file) {
         perror("Image file opening failed");
         exit (1);
     }
-    if(!label_file){
-      perror("Label file opening failed");
+    if (!label_file) {
+        perror("Label file opening failed");
         exit (1);
     }
 
@@ -146,46 +146,46 @@ void get_images(int *_num_img, unsigned char **_images, int **_labels, char *ima
     int labelNumberOfImages;
     int labelMagicNumber;
     int code;
-   
-    fseek(image_file,0,SEEK_SET); 
-    fread(&magicNumber,sizeof(int),1,image_file);
-    fread(&num_img, sizeof(int),1,image_file);
-    fread(&numberOfRows,sizeof(int),1,image_file);
-    fread(&numberOfColumns,sizeof(int),1,image_file);
 
-    fseek(label_file,0,SEEK_SET);
-    fread(&labelMagicNumber,sizeof(int),1,label_file);
-    fread(&labelNumberOfImages,sizeof(int),1,label_file);
+    fseek(image_file, 0, SEEK_SET);
+    fread(&magicNumber, sizeof(int), 1, image_file);
+    fread(&num_img, sizeof(int), 1, image_file);
+    fread(&numberOfRows, sizeof(int), 1, image_file);
+    fread(&numberOfColumns, sizeof(int), 1, image_file);
 
-    magicNumber=ReverseInt(magicNumber);
-    num_img=ReverseInt(num_img);
-    numberOfRows=ReverseInt(numberOfRows);
-    numberOfColumns=ReverseInt(numberOfColumns);
-    labelNumberOfImages=ReverseInt(labelNumberOfImages);
-    labelMagicNumber=ReverseInt(labelMagicNumber);
+    fseek(label_file, 0, SEEK_SET);
+    fread(&labelMagicNumber, sizeof(int), 1, label_file);
+    fread(&labelNumberOfImages, sizeof(int), 1, label_file);
+
+    magicNumber = ReverseInt(magicNumber);
+    num_img = ReverseInt(num_img);
+    numberOfRows = ReverseInt(numberOfRows);
+    numberOfColumns = ReverseInt(numberOfColumns);
+    labelNumberOfImages = ReverseInt(labelNumberOfImages);
+    labelMagicNumber = ReverseInt(labelMagicNumber);
 
     *_num_img = num_img;
-    
-    images = (unsigned char *) malloc(sizeof(unsigned char)* num_img * numberOfRows * numberOfColumns);
-    unsigned char * temp_labels = (unsigned char *) malloc(sizeof(int)* num_img); 
-    labels = (int *) malloc(sizeof(int)* num_img); 
 
-    code=fread(images,sizeof(unsigned char),num_img * numberOfRows* numberOfColumns,image_file);
+    images = (unsigned char *) malloc(sizeof(unsigned char) * num_img * numberOfRows * numberOfColumns);
+    unsigned char * temp_labels = (unsigned char *) malloc(sizeof(int) * num_img);
+    labels = (int *) malloc(sizeof(int) * num_img);
 
-    if(code != num_img * numberOfRows* numberOfColumns){
+    code = fread(images, sizeof(unsigned char), num_img * numberOfRows * numberOfColumns, image_file);
+
+    if (code != num_img * numberOfRows * numberOfColumns) {
         perror("Image transfer failed");
         exit (1);
     }
 
-    code = fread(temp_labels,sizeof(unsigned char),num_img,label_file);
-    if(code != labelNumberOfImages){
+    code = fread(temp_labels, sizeof(unsigned char), num_img, label_file);
+    if (code != labelNumberOfImages) {
         perror("Label transfer failed");
         exit (1);
     }
 
-    for(int i=0;i<labelNumberOfImages;i++)
+    for (int i = 0; i < labelNumberOfImages; i++)
     {
-        *(labels+i) = *(temp_labels+i);
+        *(labels + i) = *(temp_labels + i);
         //printf("%d ", *(labels+i));
     }
 
@@ -194,7 +194,7 @@ void get_images(int *_num_img, unsigned char **_images, int **_labels, char *ima
 
     fclose(image_file);
     fclose(label_file);
-    
+
 }
 
 
@@ -209,18 +209,18 @@ int * get_image(int num, int *start)
 
 /*
  Gives us lable of the image n
-*/ 
+*/
 int get_label(int num, int * labels)
 {
-    return *(labels+num);
+    return *(labels + num);
 }
 
 /*
- *Sigmoid Function 
+ *Sigmoid Function
  */
 double sigmoid(double val)
 {
-    return 1/(1+exp(-1 * val));
+    return 1 / (1 + exp(-1 * val));
 }
 
 /*
@@ -231,22 +231,22 @@ void COD_training_update(int yi, int * xi)
     //Positive Phase
     int y0 = yi;
     int * x0 = xi;
-    
+
     double * h0_cap = (double *) malloc(sizeof(double) * n);
 
-    for(int i=0; i<n ;i++)
+    for (int i = 0; i < n ; i++)
     {
         double sum = c[i];
-        for(int j = 0; j< D; j++)
+        for (int j = 0; j < D; j++)
         {
-            sum = sum + W[i*D+j] * x0[j];
+            sum = sum + W[i * D + j] * x0[j];
         }
-        
+
 
         //Can be optimized
-        for(int j = 0; j < K; j++)
+        for (int j = 0; j < K; j++)
         {
-            if(j==y0) sum = sum + U[i*K+j];
+            if (j == y0) sum = sum + U[i * K + j];
         }
 
         h0_cap[i] = sigmoid(sum);
@@ -258,79 +258,79 @@ void COD_training_update(int yi, int * xi)
     gibbs_H(h0, y0, x0);
     int y1 = gibbs_Y(h0);
     int * x1 = (int *) malloc(sizeof(int) * D);
-    gibbs_X(x1,h0);
-    
+    gibbs_X(x1, h0);
+
     double * h1_cap = (double *) malloc(sizeof(double) * n);
-    
-    for(int i=0; i<n ;i++)
+
+    for (int i = 0; i < n ; i++)
     {
         double sum = c[i];
-        for(int j = 0; j< D; j++)
+        for (int j = 0; j < D; j++)
         {
-            sum = sum + W[i*D+j] *  x1[j];
+            sum = sum + W[i * D + j] *  x1[j];
         }
-        
-        
+
+
         //Can be optimized
-        for(int j = 0; j < K; j++)
+        for (int j = 0; j < K; j++)
         {
-            if(j==y1) sum = sum + U[i*K+j];
+            if (j == y1) sum = sum + U[i * K + j];
         }
-        
+
         h1_cap[i] = sigmoid(sum);
     }
-    
-    //Update Phase
-    
+
+    //Update Phase 
+
     //Update W
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<D;j++)
+        for (int j = 0; j < D; j++)
         {
-            W[i*D+j] = W[i*D+j] + lambda * (h0_cap[i]*x0[j] - h1_cap[i]*x1[j]);
+            W[i * D + j] = W[i * D + j] + lambda * (h0_cap[i] * x0[j] - h1_cap[i] * x1[j]);
         }
     }
-              
+
     //Update b
-    for(int i=0;i<D;i++)
+    for (int i = 0; i < D; i++)
     {
         b[i] = b[i] + lambda * (x0[i] - x1[i]);
     }
-    
+
     //Update c
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         c[i] = c[i] + lambda * (h0_cap[i] - h1_cap[i]);
     }
-    
+
     //Update d
-    for (int i=0; i<K; i++)
+    for (int i = 0; i < K; i++)
     {
-        if(i==y0)
-            d[i] = d[i] + lambda * (y0);
-        
-        if(i==y1)
-            d[i] = d[i] + lambda * (- y1);
+        if (i == y0)
+            d[i] = d[i] + lambda;
+
+        if (i == y1)
+            d[i] = d[i] + lambda * (- 1);
     }
-    
+
     //Update U
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<K;j++)
+        for (int j = 0; j < K; j++)
         {
-            if(j==y0)
-                U[i*K+j] = U[i*K+j] + lambda * (h0_cap[i]);
-            
-            if(j==y1)
-                U[i*K+j] = U[i*K+j] + lambda * (-h1_cap[i]);
+            if (j == y0)
+                U[i * K + j] = U[i * K + j] + lambda * (h0_cap[i]);
+
+            if (j == y1)
+                U[i * K + j] = U[i * K + j] + lambda * (-h1_cap[i]);
         }
     }
-    
+
     free(h0_cap);
     free(h1_cap);
     free(x1);
     free(h0);
-            
+
 }
 
 /*
@@ -339,10 +339,10 @@ void COD_training_update(int yi, int * xi)
 void COD_train()
 {
     int i;
-    for(i=0;i<num_img_train;i++)
+    for (i = 0; i < num_img_train; i++)
     {
 
-        printf("training image %d\n",i+1);
+        printf("training image %d\n", i + 1);
         COD_training_update(get_label(i, labels_train), get_image(i, images_train));
 
     }
@@ -354,17 +354,17 @@ void COD_train()
 */
 void gibbs_H(int * h0, int y0, int *x0)
 {
-    for(int j=0;j<n;j++)
+    for (int j = 0; j < n; j++)
     {
-        double sum = c[j] + U[j*K+y0];
-        for(int i=0;i<D;i++)
+        double sum = c[j] + U[j * K + y0];
+        for (int i = 0; i < D; i++)
         {
-            sum = sum + W[j*D+i] * x0[j];
+            sum = sum + W[j * D + i] * x0[j];
         }
 
         double p = sigmoid(sum);
         double rand = uniform();
-        if(rand>p)
+        if (rand > p)
         {
             h0[j] = 1;
         }
@@ -375,61 +375,61 @@ void gibbs_H(int * h0, int y0, int *x0)
     }
 }
 
- /*
- Gibbs samplings for Y (label)
- Returns value for Y 
+/*
+Gibbs samplings for Y (label)
+Returns value for Y
 */
 int gibbs_Y(int* h0)
 {
     double sum = 0;
-    double *y_temp = (double *) malloc(sizeof(double)*K);
-    for(int i=0;i<K;i++)
+    double *y_temp = (double *) malloc(sizeof(double) * K);
+    for (int i = 0; i < K; i++)
     {
         double val = d[i];
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            val = val + U[j*K+i] * h0[j];
+            val = val + U[j * K + i] * h0[j];
         }
         y_temp[i] = exp(val);
         sum = sum + y_temp[i];
     }
-    
-    for(int i=0;i<K;i++)
+
+    for (int i = 0; i < K; i++)
     {
-        y_temp[i] = y_temp[i]/sum;
+        y_temp[i] = y_temp[i] / sum;
     }
-    
-    double rand = uniform();
+
+    double rand_n = uniform();
     sum = 0;
-    
-    for(int i=0;i<K;i++)
+
+    for (int i = 0; i < K; i++)
     {
         sum = sum + y_temp[i];
-        if(sum >= rand)
+        if (sum >= rand_n)
         {
             return i;
         }
     }
-    return K-1;
+    return K - 1;
 }
 
- /*
- Gibbs samplings for X (image)
- Modifies x
+/*
+Gibbs samplings for X (image)
+Modifies x
 */
 void gibbs_X(int * x, int * h0)
 {
-    for(int i=0; i<D; i++)
+    for (int i = 0; i < D; i++)
     {
         double sum = b[i];
-        for(int j=0; j<n ;j++)
+        for (int j = 0; j < n ; j++)
         {
-            sum = sum + W[j*D+i]*h0[j];
+            sum = sum + W[j * D + i] * h0[j];
         }
-        
+
         double p = sigmoid(sum);
-        double rand = uniform();
-        if(rand>p)
+        double rand_n = uniform();
+        if (rand_n > p)
         {
             x[i] = 1;
         }
@@ -440,47 +440,47 @@ void gibbs_X(int * x, int * h0)
     }
 }
 
-int Predict_one_image(int *x){
-    
+int Predict_one_image(int *x) {
+
     //W[j][i]=W[n*j+i] nxD
     /*
      calculate the term cj+\sum Wji xi
      */
-    
-    double *precomValues =(double *) malloc(sizeof(double)*n);
-    
-    
-    
-    for(int j=0; j<n;j++){
-        double sum=0;
-        for(int i=0;i<D;i++){
-            sum += W[j*D+i]*(double)x[i];
+
+    double *precomValues = (double *) malloc(sizeof(double) * n);
+
+
+
+    for (int j = 0; j < n; j++) {
+        double sum = 0;
+        for (int i = 0; i < D; i++) {
+            sum += W[j * D + i] * (double)x[i];
         }
-        precomValues[j]=c[j]+sum;
+        precomValues[j] = c[j] + sum;
     }
-    
+
     /*
      calculate the product exp(dy)*\Prod (1+Exp(Ujy+cj+\sum Wji xi))
      */
-    
-    double *numerators =(double *) malloc(sizeof(double)*K);
-    
+
+    double *numerators = (double *) malloc(sizeof(double) * K);
+
     for (int y = 0; y < K; y++) {
         double prod = 0;
         for (int j = 0; j < n; j++) {
-            
+
             prod += log((1 + exp(U[j * K + y] + precomValues[j])));
         }
         prod = d[y] + prod;
-        
+
         numerators[y] = prod;
-        
+
     }
-    
+
     /*
      calculate denominator
      */
-    
+
     // double denominator=0;
     // for(int y=0;y<K;y++){
     //     denominator+=numerators[y];
@@ -489,48 +489,48 @@ int Predict_one_image(int *x){
      return most probable class
      */
     //posible optimization, the denominator is not requiered to make the predictions
-    int mostProClass=0;
-    double max=numerators[0];
-    
-    for(int y=1;y<K;y++){
-        
-        if(max<(numerators[y])){
-            mostProClass=y;
-            max=(numerators[y]);
+    int mostProClass = 0;
+    double max = numerators[0];
+
+    for (int y = 1; y < K; y++) {
+
+        if (max < (numerators[y])) {
+            mostProClass = y;
+            max = (numerators[y]);
         }
-        
-        
+
+
     }
-    
-    
+
+
     free(precomValues);
     free(numerators);
-    
+
     return mostProClass;
-    
+
 }
 
-void predict_images (){
+void predict_images () {
 
-    predictions =(int *)malloc(sizeof(int)*num_img_test);
-for(int i=0;i<num_img_test;i++){
-    printf("predicting image %d\n",i+1);
-    predictions[i]=Predict_one_image(get_image(i, images_test));
+    predictions = (int *)malloc(sizeof(int) * num_img_test);
+    for (int i = 0; i < num_img_test; i++) {
+        printf("predicting image %d\n", i + 1);
+        predictions[i] = Predict_one_image(get_image(i, images_test));
 
     }
 
 }
 
 
-double score_function(){
+double score_function() {
 
-double score=0;
-for(int i=0;i<num_img_test;i++){
-if(predictions[i]!=labels_test[i])
-    score++;
-}
+    double score = 0;
+    for (int i = 0; i < num_img_test; i++) {
+        if (predictions[i] != labels_test[i])
+            score++;
+    }
 
-return 100*score/((double) num_img_test);
+    return 100 * score / ((double) num_img_test);
 
 }
 
@@ -538,16 +538,16 @@ return 100*score/((double) num_img_test);
 int main()
 {
     get_images(&num_img_train, &images_train_init, &labels_train,
-        "../MNISTDataSet/train-images-idx3-ubyte", "../MNISTDataSet/train-labels-idx1-ubyte");
+               "../MNISTDataSet/train-images-idx3-ubyte", "../MNISTDataSet/train-labels-idx1-ubyte");
 
     get_images(&num_img_test, &images_test_init, &labels_test,
-        "../MNISTDataSet/t10k-images-idx3-ubyte", "../MNISTDataSet/t10k-labels-idx1-ubyte");
+               "../MNISTDataSet/t10k-images-idx3-ubyte", "../MNISTDataSet/t10k-labels-idx1-ubyte");
 
     image_pp();
 
     check_images_labels(400, 222);
 
-    
+
     init_param();
     printf(" DONE Initializing W b c d and U\n");
 
@@ -555,11 +555,11 @@ int main()
 
     predict_images ();
 
-    double score =score_function();
+    double score = score_function();
 
 
 
-    printf("Misclassification error: %lf \n",score);
+    printf("Misclassification error: %lf \n", score);
 
 
 
