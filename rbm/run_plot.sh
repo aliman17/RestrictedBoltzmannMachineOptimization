6a69a8cd011b28328a381504b0f94ea104f4b7e9
psanthan <psanthan@student.ethz.ch>
2016-05-21 12:26:52 +0200

    Bug in b-update removed

--- rbm/run_plot.sh
+++ rbm/run_plot.sh
@@ -2,7 +2,6 @@
 
 echo $2 > $1/compiler_flags
 rm $1/PERF*
-#rm $1/measure
 FILE=$1/config
 while read line; do
     initial="$(echo $line | head -c 1)"
@@ -10,11 +9,10 @@ while read line; do
     then
         tag=`echo $line | sed 's/ .*//'`
         echo $tag
-        #icpc ./rbm_main.c -I./$1 -I. $2 -o $1/measure -D$tag
-        gcc-mp-4.9 -std=c99 -Wa,-q ./rbm_main.c -I./$1 -I. $2 -o $1/measure -D$tag
+        gcc ./rbm_main.c -I./$1 -I. $2 -o $1/measure -D$tag
         c=200
         printf "" > $1/$tag
-        while ((c<=800))
+        while ((c<=2000))
         do
             echo $c
             ./$1/measure $c >> $1/$tag
