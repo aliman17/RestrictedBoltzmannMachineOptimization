93e1b40208920604527b6235c671bb3c8582eb07
Aleš Omerzel <omerzel.vales@gmail.com>
2016-06-09 00:53:20 +0200

    Fix roofline

--- rbm_final_Utrans/plot_benchmarks.R
+++ rbm_final_Utrans/plot_benchmarks.R
@@ -64,18 +64,20 @@ plot_roofline <- function(name, n_koef, n_intercept, n_cost, intercept_cost, n_b
     p <- qplot((op.intensity), (perf_proc),
     xlab = "Operational Intensity [flops/byte]",
     ylab = "Performance [flops/cycle]",
-    #ylim = c(0, 100),
-    main = paste("Cycles: ", floor(cycles))) +
-    geom_text(aes(label=n))+
-    geom_line(aes((c(0, peak_performance/PI)), (c(0, peak_performance))), col = 2) +
-    geom_line(aes((c(0, 1/2)), (c(0, 1/2 * CACHE_BANDWIDTH))), col = 2) +
+    ylim = c(0, 4),
+    main = "Roofline") +
+    geom_point(col = "white") +
+    geom_text(aes(label=n), col = "blue", size = 6)+
+    geom_line(aes((c(0, peak_performance/PI/4)), (c(0, peak_performance/4))), col = 2) +
+    geom_line(aes((c(0, 1/2/4)), (c(0, 1/2 * CACHE_BANDWIDTH/4))), col = 2) +
     theme(axis.title.y = element_text(angle=y.axis.angle)) +
     geom_hline(yintercept = (peak_performance), col = 2) +
-    geom_hline(yintercept = (old_peak_performance) , col = "grey") +
-    annotate("text", label = "vec. peak performance", x = 1, y = 15.7, size = 4, colour = "red") +
-    annotate("text", label = "old peak performance", x = 1, y = 3.8, size = 4, colour = "grey") +
-    annotate("text", label = "memory bound", x = 1.22, y = 10, size = 4, colour = "red") +
-    annotate("text", label = "cache bound", x = 0.45, y = 9.4, size = 4, colour = "red")
+    geom_hline(yintercept = (old_peak_performance) , col = "red") +
+    annotate("text", label = "vec. peak performance", x = 1, y = 15.7, size = 6, colour = "red") +
+    annotate("text", label = "scalar peak performance", x = 0.7, y = 3.9, size = 7, colour = "red") +
+    annotate("text", label = "memory bound", x = 0.5, y = 3.5, size = 7, colour = "red") +
+    annotate("text", label = "cache bound", x = 0.15, y = 3.3, size = 7, colour = "red")+ theme(text = element_text(size=20))
+
     #annotate("text", label = substring(name, 6), x = 600, y = max(perf_proc)+10, size = 4, colour = "black")
     
     return(p)
@@ -116,19 +118,18 @@ plot_roofline_cache <- function(name, n_koef, n_intercept, n_cost, intercept_cos
     return(p)
 }
 
-
 # Open config file
 config <- read.table("config") 
 
 #pdf(file='plot.pdf')
 for(i in 1:nrow(config)){
   name <- as.character(config[i, 1])
-  
   # Check if the row is commented
   if (substring(name, 1, 1) == "#")
     next
   
   # Performance
+  print("Plot performance...")
   n_val <- as.numeric(config[i, 2])
   intercept <- as.numeric(config[i, 3])
   plot <- plot_perf(name, n_val, intercept)
@@ -136,8 +137,10 @@ for(i in 1:nrow(config)){
   print(plot)
   dev.off()
 
+
   # Roofline for memory
   if (ncol(config) > 3){
+    print("Roofline")
     n_cost <- as.numeric(config[i, 4])
     intercept_cost <- as.numeric(config[i, 5])
     n_byte <- as.numeric(config[i, 6])
@@ -151,6 +154,7 @@ for(i in 1:nrow(config)){
   
   # Roofline for cache
   if (ncol(config) > 7){
+    print("Cache roofline")
     n_cost <- as.numeric(config[i, 4])
     intercept_cost <- as.numeric(config[i, 5])
     n_byte <- as.numeric(config[i, 8])
